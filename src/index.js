c5f2dfc4a495535de3c3790db69a54d7a5f620c6
David Volm <david@volminator.com>
2019-12-04 14:25:01 -0600

    MIDIfighter3d support added. Maybe next detect "MIDI input connected" changes

--- src/index.js
+++ src/index.js
@@ -1,208 +1,230 @@
-r(function() {
-	var DEBUG = false,
-		alertOne = true;
+(function() {
+	var _r = function(useMidi) {
+		var DEBUG = false,
+			alertOne = true;
 
-	// create a wrapper around native canvas element (with id="c")
-	var canvas = new fabric.StaticCanvas('c');
+		// create a wrapper around native canvas element (with id="c")
+		var canvas = new fabric.StaticCanvas('c');
 
-	// handle mouse events on canvas
-	var mh = handleMouse('c');
+		// handle mouse events on canvas
+		var mh = handleMouse('c');
 
-	// create background
-	Game.bg = new fabric.Rect({
-	  left: 0,
-	  top: 0,
-	  fill: 'white',
-	  width: Game.w,
-	  height: Game.h
-	});
-
-	// add it to the canvas
-	canvas.add(Game.bg);
-	
-	//debug accel line
-	if(DEBUG) {
-		var line = new fabric.Line([0, 0, 40, 40], {
-		  stroke: 'green'
-		});
-
-		canvas.add(line);
-	}
-
-	var aLine = new Game.Line(0,0,0,0);
-
-	//when we finish a level
-	var onWin = function() {
-		console.log('level finished: ' + Game.level);
-		Game.clear();
-
-		if(Game.level === Game.levels.length) {
-			Game.level = 0; //wrap around
-		} else {
-			Game.level++;
+		// handle MidiFighter3D if we have it
+		var	midiH = false;
+		if(useMidi === true) {
+			midiH = handleMidi();
 		}
 
-		Game.initLevel(Game.levels[Game.level]);
-	};
+		console.log(midiH);
 
-	Game.instawin = onWin; //incase we wanna cheat past a level using the JS console
-
-	Game.initLevel = function(_Maze) {
-	 	// create a ball object
-		var ballInit = Game.findID(_Maze, 2),
-			ballSize = 16;
-
-		Game.obj.ball = new Game.object(canvas, {
-			x: ballInit.x + (ballSize / 2),
-			y: ballInit.y + (ballSize / 2),
-			width: ballSize,
-			height: ballSize,
-			solid: true
+		// create background
+		Game.bg = new fabric.Rect({
+		  left: 0,
+		  top: 0,
+		  fill: 'white',
+		  width: Game.w,
+		  height: Game.h
 		});
 
-			// debug bounding box
-			if(DEBUG) {
-				Game.obj.ball.add(new fabric.Rect({
-				  left: Game.obj.ball.x,
-				  top: Game.obj.ball.y,
-				  fill: 'red',
-				  width: Game.obj.ball.width,
-				  height: Game.obj.ball.height
-				}));
+		// add it to the canvas
+		canvas.add(Game.bg);
+		
+		//debug accel line
+		if(DEBUG) {
+			var line = new fabric.Line([0, 0, 40, 40], {
+			  stroke: 'green'
+			});
+
+			canvas.add(line);
+		}
+
+		var aLine = new Game.Line(0,0,0,0);
+
+		// when we finish a level
+		var onWin = function() {
+			console.log('level finished: ' + Game.level);
+			Game.clear();
+
+			if(Game.level === Game.levels.length) {
+				Game.level = 0; //wrap around
+			} else {
+				Game.level++;
 			}
 
-			// a circle
-			Game.obj.ball.add(new fabric.Circle({
-			  left: Game.obj.ball.x,
-			  top: Game.obj.ball.y,
-			  fill: 'black',
-			  radius: Game.obj.ball.width / 2
-			})).setPreDraw(function() {
-				aLine = new Game.Line(Game.obj.ball.x, Game.obj.ball.y, mh.mouse.x, mh.mouse.y);
-				if(DEBUG) {
-					line.set(aLine.getSetter());
-				}
+			Game.initLevel(Game.levels[Game.level]);
+		};
 
-				this.xAccel = -((aLine.x1 - aLine.x2)/300);
-				this.yAccel = -((aLine.y1 - aLine.y2)/300);
-				this.friction = 0.2;
-			}).setPostDraw(function() {
-			}).save();
+		Game.instawin = onWin; //incase we wanna cheat past a level using the JS console
 
-		// create an objective object
-		var objectiveInit = Game.findID(_Maze, 3);
+		Game.initLevel = function(_Maze) {
+		 	// create a ball object
+			var ballInit = Game.findID(_Maze, 2),
+				ballSize = 16;
 
-		Game.obj.objective = new Game.object(canvas, {
-			x: objectiveInit.x,
-			y: objectiveInit.y,
-			width: 16,
-			height: 16,
-			solid: true
-		});
-
-			// debug bounding box
-			if(DEBUG) {
-				Game.obj.objective.add(new fabric.Rect({
-				  left: Game.obj.objective.x,
-				  top: Game.obj.objective.y,
-				  fill: 'red',
-				  width: Game.obj.objective.width,
-				  height: Game.obj.objective.height
-				}));
-			}
-
-		Game.obj.objective.add(new fabric.Circle({
-			left: Game.obj.objective.x,
-			top: Game.obj.objective.y,
-			fill: 'green',
-			radius: Game.obj.objective.width / 2
-		})).onIntersect(Game.obj.ball, function() {
-			Game.addEvent(onWin);
-		});
-
-		// create an hazard object
-		Game.findIDs(_Maze, 4).forEach(function(hazardInit, i) {
-			Game.obj["hazard_" + i] = new Game.object(canvas, {
-				x: hazardInit.x,
-				y: hazardInit.y,
-				width: 32,
-				height: 32,
+			Game.obj.ball = new Game.object(canvas, {
+				x: ballInit.x + (ballSize / 2),
+				y: ballInit.y + (ballSize / 2),
+				width: ballSize,
+				height: ballSize,
 				solid: true
 			});
 
 				// debug bounding box
 				if(DEBUG) {
-					Game.obj["hazard_" + i].add(new fabric.Rect({
-					  left: Game.obj["hazard_" + i].x,
-					  top: Game.obj["hazard_" + i].y,
-					  fill: 'green',
-					  width: Game.obj["hazard_" + i].width,
-					  height: Game.obj["hazard_" + i].height
+					Game.obj.ball.add(new fabric.Rect({
+					  left: Game.obj.ball.x,
+					  top: Game.obj.ball.y,
+					  fill: 'red',
+					  width: Game.obj.ball.width,
+					  height: Game.obj.ball.height
 					}));
 				}
 
-			Game.obj["hazard_" + i].add(new fabric.Circle({
-				left: Game.obj["hazard_" + i].x,
-				top: Game.obj["hazard_" + i].y,
-				fill: 'red',
-				radius: Game.obj["hazard_" + i].width / 2
-			})).onIntersect(Game.obj.ball, function() {
-				Game.obj.ball.reset();
-			});
-		});
+				// a circle
+				Game.obj.ball.add(new fabric.Circle({
+				  left: Game.obj.ball.x,
+				  top: Game.obj.ball.y,
+				  fill: (midiH === false) ? 'black' : 'blue', // if we are using midi ball is blue
+				  radius: Game.obj.ball.width / 2
+				})).setPreDraw(function() {
+					if(midiH === false) { // no midi, fallback to mouse
+						aLine = new Game.Line(Game.obj.ball.x, Game.obj.ball.y, mh.mouse.x, mh.mouse.y);
+						if(DEBUG) {
+							line.set(aLine.getSetter());
+						}
 
-		// walls
-		Game.buildMaze(canvas, _Maze);
+						this.xAccel = -((aLine.x1 - aLine.x2)/300);
+						this.yAccel = -((aLine.y1 - aLine.y2)/300);
+						this.friction = 0.2;
+					} else { // using midi :)
+						this.xAccel = midiH.x / 128;
+						this.yAccel = midiH.y / 128;
+						this.friction = 0.1;
+					}
+				}).setPostDraw(function() {
+				}).save();
+
+			// create an objective object
+			var objectiveInit = Game.findID(_Maze, 3);
+
+			Game.obj.objective = new Game.object(canvas, {
+				x: objectiveInit.x,
+				y: objectiveInit.y,
+				width: 16,
+				height: 16,
+				solid: true
+			});
+
+				// debug bounding box
+				if(DEBUG) {
+					Game.obj.objective.add(new fabric.Rect({
+					  left: Game.obj.objective.x,
+					  top: Game.obj.objective.y,
+					  fill: 'red',
+					  width: Game.obj.objective.width,
+					  height: Game.obj.objective.height
+					}));
+				}
+
+			Game.obj.objective.add(new fabric.Circle({
+				left: Game.obj.objective.x,
+				top: Game.obj.objective.y,
+				fill: 'green',
+				radius: Game.obj.objective.width / 2
+			})).onIntersect(Game.obj.ball, function() {
+				Game.addEvent(onWin);
+			});
+
+			// create an hazard object
+			Game.findIDs(_Maze, 4).forEach(function(hazardInit, i) {
+				Game.obj["hazard_" + i] = new Game.object(canvas, {
+					x: hazardInit.x,
+					y: hazardInit.y,
+					width: 32,
+					height: 32,
+					solid: true
+				});
+
+					// debug bounding box
+					if(DEBUG) {
+						Game.obj["hazard_" + i].add(new fabric.Rect({
+						  left: Game.obj["hazard_" + i].x,
+						  top: Game.obj["hazard_" + i].y,
+						  fill: 'green',
+						  width: Game.obj["hazard_" + i].width,
+						  height: Game.obj["hazard_" + i].height
+						}));
+					}
+
+				Game.obj["hazard_" + i].add(new fabric.Circle({
+					left: Game.obj["hazard_" + i].x,
+					top: Game.obj["hazard_" + i].y,
+					fill: 'red',
+					radius: Game.obj["hazard_" + i].width / 2
+				})).onIntersect(Game.obj.ball, function() {
+					Game.obj.ball.reset();
+				});
+			});
+
+			// walls
+			Game.buildMaze(canvas, _Maze);
+		};
+
+		Game.levels.length = window.Maze.length;
+
+		for(var key in window.Maze) {
+			((function(key, value, object) {
+				if(key !== "length") {
+					var xhr = new XMLHttpRequest();
+					xhr.open('GET', value, true);
+					xhr.responseType = 'arraybuffer';
+
+					xhr.onload = function(e){
+						if (this.status == 200){
+							var reader = new PNGReader(this.response);
+							reader.parse(function(err, png){
+								if (err) throw err;
+								Game.levels[key] = Game.convertPixels(png);
+
+								if((+key) === 0) { //if the first level, load it
+									Game.initLevel(Game.levels[key]);
+								}
+							});
+						}
+					};
+
+					xhr.send();
+				}
+			})(key, window.Maze[key], window.Maze));
+		}
+
+		var tick = function() {
+			for(var objName in Game.obj) {
+				if(objName !== 'walls') {
+					Game.obj[objName].preDraw();
+					Game.obj[objName].draw();
+				}
+			}
+
+			canvas.renderAll();
+
+			for(var objName in Game.obj) {
+				if(objName !== 'walls') {
+					Game.obj[objName].postDraw();
+				}
+			}
+
+			Game.processEvents(); //do events after we have done all the stuff with objects
+
+			Game.ticks++;
+
+			setTimeout(tick, Game.tickspace);
+		}; setTimeout(tick, Game.tickspace);
 	};
 
-	Game.levels.length = window.Maze.length;
-
-	for(var key in window.Maze) {
-		((function(key, value, object) {
-			if(key !== "length") {
-				var xhr = new XMLHttpRequest();
-				xhr.open('GET', value, true);
-				xhr.responseType = 'arraybuffer';
-
-				xhr.onload = function(e){
-					if (this.status == 200){
-						var reader = new PNGReader(this.response);
-						reader.parse(function(err, png){
-							if (err) throw err;
-							Game.levels[key] = Game.convertPixels(png);
-
-							if((+key) === 0) { //if the first level, load it
-								Game.initLevel(Game.levels[key]);
-							}
-						});
-					}
-				};
-
-				xhr.send();
-			}
-		})(key, window.Maze[key], window.Maze));
-	}
-
-	var tick = function() {
-		for(var objName in Game.obj) {
-			if(objName !== 'walls') {
-				Game.obj[objName].preDraw();
-				Game.obj[objName].draw();
-			}
-		}
-
-		canvas.renderAll();
-
-		for(var objName in Game.obj) {
-			if(objName !== 'walls') {
-				Game.obj[objName].postDraw();
-			}
-		}
-
-		Game.processEvents(); //do events after we have done all the stuff with objects
-
-		Game.ticks++;
-
-		setTimeout(tick, Game.tickspace);
-	}; setTimeout(tick, Game.tickspace);
-});
+	r(function() {
+		MIDI.request().then(function() {
+			_r(true);
+		}, _r)
+	});
+})();
--- src/midi.js
+++ src/midi.js
@@ -1,45 +1,86 @@
 var handleMidi = function() {
     var inputList =  MIDI.inputs().entries(),
-        notDone = true,
-        nextInput = {},
         lookingForManufacturer = "www.djtechtools.com",
-        foundMF = {};
+        foundMF = false;
     
+    var MidiFighterTilt = function() {
+        this.left = 0;
+        this.up = 0;
+        this.down = 0;
+        this.right = 0;
+        this.x = 0;
+        this.y = 0;
+        this.flist = [];
+    };
+
+    MidiFighterTilt.prototype.on = function(fx) {
+        this.flist.push(fx);
+    };
+
+    MidiFighterTilt.prototype._process_events = function() {
+        var that = this;
+
+        this.x = (this.left > this.right) ? -this.left : this.right;
+        this.y = (this.up > this.down) ? -this.up : this.down;
+
+        this.flist.forEach(function(v,i){v(that,i);});
+    };
+
     var withEachMidiInput = function(input) {
         if(input.manufacturer === lookingForManufacturer) {
             foundMF = input.id;
         }
     };
 
-    while(notDone) {
-        nextInput = inputList.next();
-        notDone = !nextInput.done;
+    var nextInput = inputList.next();
 
-        if(notDone) {
+    while(!nextInput.done) {
+        nextInput = inputList.next();
+
+        if(typeof nextInput.value !== 'undefined') {
+            console.log(nextInput.value);
             withEachMidiInput(nextInput.value[1], nextInput.value[0]);
         }
     }
 
-    MIDI.getInput(foundMF).onmidimessage = function(fx) {
-        var data = fx.data;
+    console.log(nextInput);
 
-        switch(data[0]) {
-            case 179:
-                switch(data[1]) {
-                    case 0: // tilt left
-                        console.log('left', data[2]);
-                      break;
-                    case 1: // tilt up
-                        console.log('up', data[2]);
-                      break;
-                    case 2: // tilt right
-                        console.log('right', data[2]);
-                      break;
-                    case 3:  // tilt down
-                        console.log('down', data[2]);
-                      break;
-                }
-              break;
-        }
-    };
+    if(foundMF !== false) {
+        var returnCTX = new MidiFighterTilt();
+
+        MIDI.getInput(foundMF).onmidimessage = function(fx) {
+            var data = fx.data;
+
+            switch(data[0]) {
+                case 179:
+                    switch(data[1]) {
+                        case 0: // tilt left
+                            //console.log('left', data[2]);
+                            returnCTX.left = data[2];
+                            returnCTX._process_events();
+                          break;
+                        case 1: // tilt up
+                            //console.log('up', data[2]);
+                            returnCTX.up = data[2];
+                            returnCTX._process_events();
+                          break;
+                        case 2: // tilt right
+                            //console.log('right', data[2]);
+                            returnCTX.right = data[2];
+                            returnCTX._process_events();
+                          break;
+                        case 3:  // tilt down
+                            //console.log('down', data[2]);
+                            returnCTX.down = data[2];
+                            returnCTX._process_events();
+                          break;
+                    }
+                  break;
+            }
+        };
+
+        return returnCTX;
+    } else {
+        return false;
+    }
 };
